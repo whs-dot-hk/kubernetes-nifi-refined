d0f95ae8d402f3aae76f7945b48f8821e50c429c
whs <henrywong@cherrypicks.com>
2019-03-22 18:20:48 +0800

    Add cluster template

--- nifi/start-patched.sh
+++ nifi/start-patched.sh
@@ -2,4 +2,6 @@
 
 [ ! -f conf/nifi.properties ] && cp -a nifi-1.8.0/conf .
 
+[ ! -z $KUBERNETES_HEADLESS_SERVICE_NAME ] && HOSTNAME=$HOSTNAME.$KUBERNETES_HEADLESS_SERVICE_NAME
+
 ../scripts/start.sh
